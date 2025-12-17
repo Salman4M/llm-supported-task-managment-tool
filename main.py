1c3b20cc63919324e0088cd3a41d2ee54c184439
murtuzavalicanov <murtuza.velicanov14@gmail.com>
2025-12-17 10:22:32 +0400

    teams_router added to main.py

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from fastapi import FastAPI
 from users.routes.routes_v1 import router as users_router
 from projects.routes.project_routes_v1 import router as projects_router
 from reports.routes.routes_v1 import router as reports_router
+from teams.routes.routes_v1 import router as teams_router
 
 
 from users.models import models_v1
 from projects.models import models_v1
 from reports.models import models_v1
+from teams.models import models_v1
+
 
 app = FastAPI(title="LLM Task Manager")
 
@@ -15,6 +18,8 @@ app = FastAPI(title="LLM Task Manager")
 app.include_router(users_router, prefix="/users")
 app.include_router(projects_router, prefix="/projects")
 app.include_router(reports_router, prefix="/reports")
+app.include_router(teams_router, prefix="/teams")
+
 
 # def main():
 #     print("Hello from llm-app!")
